6982074fb4874fa08889cc482768fe3374d61a87
Wahyu Ramadhan 18-077 <wahyu.wrseven@gmail.com>
2021-06-08 21:11:26 +0800

    Added login authentication and registration feature

--- app/src/main/java/com/irise/damagedetection/LoginActivity.kt
+++ app/src/main/java/com/irise/damagedetection/LoginActivity.kt
@@ -1,25 +1,70 @@
 package com.irise.damagedetection
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.irise.damagedetection.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+        login()
+
+        binding.tvNext.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun login() {
         with(binding) {
             tvBtnLogin.setOnClickListener {
-                val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                startActivity(intent)
-            }
-            tvNext.setOnClickListener {
-                val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
-                startActivity(intent)
+                when {
+                    TextUtils.isEmpty(inputEmail.text.toString()) -> {
+                        inputEmail.error = resources.getString(R.string.fill)
+                        return@setOnClickListener
+                    }
+                    TextUtils.isEmpty(inputPassword.text.toString()) -> {
+                        inputPassword.error = resources.getString(R.string.fill)
+                        return@setOnClickListener
+                    }
+                    else -> {
+                        auth.signInWithEmailAndPassword(
+                            inputEmail.text.toString(),
+                            inputPassword.text.toString()
+                        )
+                            .addOnCompleteListener {
+                                if (it.isSuccessful) {
+                                    Toast.makeText(
+                                        this@LoginActivity,
+                                        resources.getString(R.string.login_success),
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                    val intent =
+                                        Intent(this@LoginActivity, MainActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                } else {
+                                    Toast.makeText(
+                                        this@LoginActivity,
+                                        resources.getString(R.string.login_failed),
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                            }
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/irise/damagedetection/RegisterActivity.kt
+++ app/src/main/java/com/irise/damagedetection/RegisterActivity.kt
@@ -2,18 +2,83 @@ package com.irise.damagedetection
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.irise.damagedetection.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    var dbReference: DatabaseReference? = null
+    var db: FirebaseDatabase? = null
     private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseDatabase.getInstance()
+        dbReference = db?.reference!!.child("profile")
+
+        register()
+
         binding.tvNext.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
+    }
+
+    private fun register() {
+        binding.tvRegister.setOnClickListener {
+            with(binding) {
+                when {
+                    TextUtils.isEmpty(inputEmail.text.toString()) -> {
+                        inputEmail.error = resources.getString(R.string.fill)
+                        return@setOnClickListener
+                    }
+                    TextUtils.isEmpty(inputUsername.text.toString()) -> {
+                        inputUsername.error = resources.getString(R.string.fill)
+                        return@setOnClickListener
+                    }
+                    TextUtils.isEmpty(inputPassword.text.toString()) -> {
+                        inputPassword.error = resources.getString(R.string.fill)
+                        return@setOnClickListener
+                    }
+                    else -> auth.createUserWithEmailAndPassword(
+                        inputEmail.text.toString(),
+                        inputPassword.text.toString()
+                    )
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                val currentUser = auth.currentUser
+                                val currentDb = dbReference?.child(currentUser?.uid!!)
+                                currentDb?.child("email")?.setValue(inputEmail.text.toString())
+                                currentDb?.child("username")
+                                    ?.setValue(inputUsername.text.toString())
+                                currentDb?.child("password")
+                                    ?.setValue(inputPassword.text.toString())
+                                Toast.makeText(
+                                    this@RegisterActivity,
+                                    resources.getString(R.string.register_success),
+                                    Toast.LENGTH_LONG
+                                ).show()
+                                finish()
+                            } else {
+                                Toast.makeText(
+                                    this@RegisterActivity,
+                                    resources.getString(R.string.register_failed),
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                }
+            }
+
+        }
+
     }
 }
